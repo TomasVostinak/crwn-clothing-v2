a89dc2d5a93e3edc5da55211289e604e2e480979
Tomáš Vostiňák <tomas.vostinak@tul.cz>
2025-08-08 11:35:36 +0200

    Created authentication and sign in form components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Home from './routes/home/home.component';
 import NavBar from './routes/navigation/navigation.component';
-import SignIn from './routes/sign-in/sign-in.component';
+import Authentication from './routes/authentication/authentication.component';
 import { Routes, Route } from 'react-router-dom';
 
 const Shop = () => {
@@ -15,7 +15,7 @@ const App = () => {
       <Route path='/' element={<NavBar />}>
         <Route index element={<Home />} />
         <Route path='shop' element={<Shop />} />
-        <Route path='signin' element={<SignIn />} />
+        <Route path='auth' element={<Authentication />} />
       </Route>
     </Routes>
   )
